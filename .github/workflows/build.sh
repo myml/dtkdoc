8d641bcd95fbcb084326a1a044a14ef0a1a8abd4
myml <wurongjie@deepin.org>
2023-01-13 15:13:41 +0800

    feat: add dtk module

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,3 +1,6 @@
+# remove mainpage tag
+grep -rl '\mainpage' | xargs sed -i 's/\\mainpage//g'
+grep -rl '@mainpage' | xargs sed -i 's/@mainpage//g'
 cmake -Bbuild -DBUILD_DOCS=ON -DBUILD_TESTING=OFF -DDOXYGEN_GENERATE_HTML="YES" -DDOXYGEN_PROJECT_NUMBER=`git describe --tags` -DDOXYGEN_GENERATE_TREEVIEW="YES" \
             -DDOXYGEN_HTML_EXTRA_STYLESHEET="./doxygen-theme/doxygen-awesome-css/doxygen-awesome.css;\
                 ./doxygen-theme/doxygen-awesome-css/doxygen-awesome-sidebar-only.css;\
@@ -14,3 +17,6 @@ cmake -Bbuild -DBUILD_DOCS=ON -DBUILD_TESTING=OFF -DDOXYGEN_GENERATE_HTML="YES" 
             -DDOXYGEN_HTML_FOOTER="./doxygen-theme/doxygen-awesome-css/footer.html"\
             -DDOXYGEN_TAGFILES="qt/qtcore.tags=http://doc.qt.io/qt-5/;qt/qtwidgets.tags=http://doc.qt.io/qt-5/;qt/qtgui.tags=http://doc.qt.io/qt-5/;qt/qtnetwork.tags=http://doc.qt.io/qt-5/" .
 cmake --build build --target doxygen
+
+# reset submodule
+# git submodule status | awk '{print $2}' | xargs -i sh -c "cd {};git reset --hard HEAD"
